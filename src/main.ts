8695e9344fcfb8e3000f6b8539f5688b1147f6f2
Kubrick Code <kubrickcode@gmail.com>
2023-06-21 21:12:00 +0900

    test: 클라이언트 통신 테스트용 API 구성

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const origin = configService.get<string>('ORIGIN');
+  const corsOptions = {
+    origin,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+  app.setGlobalPrefix('api');
   await app.listen(3000);
-}
+};
 bootstrap();
