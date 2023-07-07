a9b8a9e16904e4624c0ac344a19924da16404ec8
Kubrick Code <kubrickcode@gmail.com>
2023-07-07 12:30:46 +0900

    feat: 패스워드 핸들러 도메인,인프라 구성

--- src/domain/user.service.ts
+++ src/domain/user.service.ts
@@ -1,20 +1,24 @@
-// src/domain/user/UserService.ts
-
 import { IUserRepository } from './user/interfaces/iuser.repository';
-import { User } from './user/entities/user.entity';
+import { UserEntity } from './user/entities/user.entity';
+import { IPasswordHasher } from './user/interfaces/ipasswordHasher.repository';
 
 export class UserService {
-  constructor(private userRepository: IUserRepository) {}
+  constructor(
+    private readonly userRepository: IUserRepository,
+    private readonly passwordHasher: IPasswordHasher,
+  ) {}
 
-  async createUser(email: string, password: string): Promise<User> {
-    let user = await this.userRepository.findByEmail(email);
+  async createUser(email: string, password: string): Promise<UserEntity> {
+    const hashedPassword = await this.passwordHasher.hashPassword(password);
 
-    if (user) {
-      throw new Error('이미 존재하는 계정입니다');
-    }
-
-    user = User.createNew(email, password);
+    const user = new UserEntity();
+    user.email = email;
+    user.password = hashedPassword;
 
     return this.userRepository.createUser(user);
+  }
+
+  async findByEmail(email: string): Promise<UserEntity | null> {
+    return this.userRepository.findByEmail(email);
   }
 }
--- src/domain/user/entities/user.entity.ts
+++ src/domain/user/entities/user.entity.ts
@@ -1,16 +1,14 @@
 import { User, Provider, Role } from '@prisma/client';
-import { Exclude, Transform } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UserEntity implements User {
   @ApiProperty()
-  @Transform((value) => value.toString())
   id: string;
 
   @ApiProperty()
   email: string;
 
-  @Exclude()
+  @ApiProperty()
   password: string;
 
   @ApiProperty()
