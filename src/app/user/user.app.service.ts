612a9c66a7a77efa9d6f300c2bb328078c39c2e0
Kubrick Code <kubrickcode@gmail.com>
2023-07-07 20:51:36 +0900

    fix: 유저 애플리케이션 레이어 서비스 수정

--- src/app/user/user.app.service.ts
+++ src/app/user/user.app.service.ts
@@ -1,20 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '@domain/user.service';
-import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from './dto/register.dto';
 
 @Injectable()
 export class UserAppService {
-  constructor(
-    private readonly userService: UserService,
-    private readonly jwtService: JwtService,
-  ) {}
+  constructor(private readonly userService: UserService) {}
 
-  async registerUser(createUserDto: CreateUserDto): Promise<string> {
+  async registerUser(
+    createUserDto: CreateUserDto,
+  ): Promise<{ message: string }> {
     const { email, password } = createUserDto;
-    const user = await this.userService.createUser(email, password);
+    await this.userService.createUser(email, password);
 
-    const payload = { id: user.id, email: user.email, role: user.role };
-    return this.jwtService.sign(payload);
+    return { message: '회원가입 성공' };
   }
 }
