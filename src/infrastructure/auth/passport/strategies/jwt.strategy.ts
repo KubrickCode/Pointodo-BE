393b49b1fac21d5c90fbd88a66a8e181ae05b5f1
Kubrick Code <kubrickcode@gmail.com>
2023-07-08 17:15:30 +0900

    feat: jwt 엑세스 토큰 인증 헤더로 변경

--- src/infrastructure/auth/passport/strategies/jwt.strategy.ts
+++ src/infrastructure/auth/passport/strategies/jwt.strategy.ts
@@ -1,22 +1,15 @@
-import { Strategy } from 'passport-jwt';
+import { Strategy, ExtractJwt } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { DecodedToken } from '@domain/auth/interfaces/decodedToken';
 import { ConfigService } from '@nestjs/config';
-import { Request } from 'express';
 import { jwtConfig } from 'config/jwt.config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
     super({
-      jwtFromRequest: (req: Request) => {
-        let token = null;
-        if (req && req.cookies) {
-          token = req.cookies.accessToken;
-        }
-        return token;
-      },
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: jwtConfig(configService).accessTokenSecret,
     });
