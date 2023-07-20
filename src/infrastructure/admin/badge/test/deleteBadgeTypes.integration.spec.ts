43ede8b311a46651562dd4bf16e8b70d0a104c67
Kubrick Code <kubrickcode@gmail.com>
2023-07-21 02:23:25 +0900

    feat: 뱃지 타입 삭제 리포지토리 테스트 코드 작성

---
+++ src/infrastructure/admin/badge/test/deleteBadgeTypes.integration.spec.ts
@@ -0,0 +1,33 @@
+import { PrismaService } from '@shared/services/prisma.service';
+import { BadgeAdminRepository } from '../prisma/badge.admin.repository';
+
+describe('deleteBadgeType', () => {
+  let prisma: PrismaService;
+  let badgeAdminRepository: BadgeAdminRepository;
+
+  beforeAll(() => {
+    prisma = new PrismaService();
+    badgeAdminRepository = new BadgeAdminRepository(prisma);
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+
+  it('뱃지 타입 삭제 In DB', async () => {
+    const badgeId = 1;
+
+    const retrievedBadgeType = await prisma.badgeTypes.findUnique({
+      where: { id: badgeId },
+    });
+
+    const deletedBadgeType = await badgeAdminRepository.delete(badgeId);
+
+    expect(retrievedBadgeType).toMatchObject({
+      id: deletedBadgeType.id,
+      name: deletedBadgeType.name,
+      description: deletedBadgeType.description,
+      icon: deletedBadgeType.icon,
+    });
+  });
+});
