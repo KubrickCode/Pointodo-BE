c5b5becc649ad5a03e0feef20c6326eaa909b76f
Kubrick Code <kubrickcode@gmail.com>
2023-07-21 02:51:17 +0900

    feat: 애플리케이션 레이어 어드민 뱃지타입 서비스 구성

---
+++ src/app/admin/badge/badge.admin.service.ts
@@ -0,0 +1,44 @@
+import {
+  ReqCreateBadgeTypeAppDto,
+  ResCreateBadgeTypeAppDto,
+} from '@domain/admin/badge/dto/createBadgeType.app.dto';
+import { ResDeleteBadgeTypeAppDto } from '@domain/admin/badge/dto/deleteBadgeType.app.dto';
+import {
+  ReqUpdateBadgeTypeAppDto,
+  ResUpdateBadgeTypeAppDto,
+} from '@domain/admin/badge/dto/updateBadgeType.app.dto';
+import { IBadgeAdminRepository } from '@domain/admin/badge/interfaces/badge.admin.repository.interface';
+import { IBadgeAdminService } from '@domain/admin/badge/interfaces/badge.admin.service.interface';
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  CREATE_BADGE_TYPE_SUCCESS_MESSAGE,
+  DELETE_BADGE_TYPE_SUCCESS_MESSAGE,
+  UPDATE_BADGE_TYPE_SUCCESS_MESSAGE,
+} from '@shared/messages/admin/badge.admin.messages';
+
+@Injectable()
+export class BadgeAdminService implements IBadgeAdminService {
+  constructor(
+    @Inject('IBadgeAdminRepository')
+    private readonly badgeAdminRepository: IBadgeAdminRepository,
+  ) {}
+
+  async createBadgeType(
+    req: ReqCreateBadgeTypeAppDto,
+  ): Promise<ResCreateBadgeTypeAppDto> {
+    await this.badgeAdminRepository.create(req);
+    return { message: CREATE_BADGE_TYPE_SUCCESS_MESSAGE };
+  }
+
+  async updateBadgeType(
+    req: ReqUpdateBadgeTypeAppDto,
+  ): Promise<ResUpdateBadgeTypeAppDto> {
+    await this.badgeAdminRepository.update(req);
+    return { message: UPDATE_BADGE_TYPE_SUCCESS_MESSAGE };
+  }
+
+  async deleteBadgeType(id: number): Promise<ResDeleteBadgeTypeAppDto> {
+    await this.badgeAdminRepository.delete(id);
+    return { message: DELETE_BADGE_TYPE_SUCCESS_MESSAGE };
+  }
+}
