7b5c46c35f1ef47d98b214e20ac7270a8ebde265
Kubrick Code <kubrickcode@gmail.com>
2023-07-24 03:04:48 +0900

    feat: 작업 유형 서비스 및 dto 등 구성 완료

---
+++ src/domain/admin/task/dto/updateTaskType.app.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsOptional, IsString } from 'class-validator';
+
+class ReqUpdateTaskTypeAppDto {
+  @ApiProperty({ description: '작업 타입 고유 ID(SMALL INT)' })
+  @IsInt()
+  readonly id: number;
+
+  @ApiProperty({ description: '새 작업 ID' })
+  @IsOptional()
+  @IsInt()
+  readonly newId: number;
+
+  @ApiProperty({ description: '작업 타입 이름' })
+  @IsOptional()
+  @IsString()
+  readonly name: string;
+}
+
+class ResUpdateTaskTypeAppDto {
+  @ApiProperty({
+    example: '작업 타입 업데이트 성공',
+    description: '성공 메시지',
+  })
+  @IsString()
+  readonly message: string;
+}
+
+export { ReqUpdateTaskTypeAppDto, ResUpdateTaskTypeAppDto };
