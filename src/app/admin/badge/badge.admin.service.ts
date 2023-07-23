48fa19781b715f111f7c11ab516a94a08c589605
Kubrick Code <kubrickcode@gmail.com>
2023-07-24 02:22:04 +0900

    feat: 어드민 뱃지타입 서비스 로그 추가

--- src/app/admin/badge/badge.admin.service.ts
+++ src/app/admin/badge/badge.admin.service.ts
@@ -10,18 +10,20 @@ import {
 import { BadgeTypesEntity } from '@domain/admin/badge/entities/badgeTypes.entity';
 import { IBadgeAdminRepository } from '@domain/admin/badge/interfaces/badge.admin.repository.interface';
 import { IBadgeAdminService } from '@domain/admin/badge/interfaces/badge.admin.service.interface';
-import { Inject, Injectable, ConflictException } from '@nestjs/common';
+import { Inject, Injectable, ConflictException, Logger } from '@nestjs/common';
 import {
   CREATE_BADGE_TYPE_SUCCESS_MESSAGE,
   DELETE_BADGE_TYPE_SUCCESS_MESSAGE,
   UPDATE_BADGE_TYPE_SUCCESS_MESSAGE,
 } from '@shared/messages/admin/badge.admin.messages';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 
 @Injectable()
 export class BadgeAdminService implements IBadgeAdminService {
   constructor(
     @Inject('IBadgeAdminRepository')
     private readonly badgeAdminRepository: IBadgeAdminRepository,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
   ) {}
 
   async getAllBadgeTypes(): Promise<BadgeTypesEntity[]> {
@@ -34,7 +36,11 @@ export class BadgeAdminService implements IBadgeAdminService {
     const { id, name } = req;
     const isExist = await this.badgeAdminRepository.isExist({ id, name });
     if (isExist) throw new ConflictException('이미 존재하는 ID 혹은 뱃지 이름');
-    await this.badgeAdminRepository.create(req);
+    const createdBadgeType = await this.badgeAdminRepository.create(req);
+    this.logger.log(
+      'info',
+      `생성 뱃지 타입 ID:${createdBadgeType.id}, 뱃지명:${createdBadgeType.name}, 설명:${createdBadgeType.description}, 아이콘 링크:${createdBadgeType.iconLink}`,
+    );
     return { message: CREATE_BADGE_TYPE_SUCCESS_MESSAGE };
   }
 
@@ -47,12 +53,17 @@ export class BadgeAdminService implements IBadgeAdminService {
       name,
     });
     if (isExist) throw new ConflictException('이미 존재하는 ID 혹은 뱃지 이름');
-    await this.badgeAdminRepository.update(req);
+    const updatedBadgeType = await this.badgeAdminRepository.update(req);
+    this.logger.log(
+      'info',
+      `업데이트 뱃지 타입 ID:${updatedBadgeType.id}, 뱃지명:${updatedBadgeType.name}, 설명:${updatedBadgeType.description}, 아이콘 링크:${updatedBadgeType.iconLink}`,
+    );
     return { message: UPDATE_BADGE_TYPE_SUCCESS_MESSAGE };
   }
 
   async deleteBadgeType(id: number): Promise<ResDeleteBadgeTypeAppDto> {
-    await this.badgeAdminRepository.delete(id);
+    const deletedBadgeType = await this.badgeAdminRepository.delete(id);
+    this.logger.log('info', `삭제 뱃지 타입 ID:${deletedBadgeType.id}`);
     return { message: DELETE_BADGE_TYPE_SUCCESS_MESSAGE };
   }
 }
