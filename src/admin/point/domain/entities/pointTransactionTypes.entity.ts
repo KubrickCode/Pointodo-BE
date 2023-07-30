8dd2a79b41555ae194cc7a046bee04639d8c6c10
Kubrick Code <kubrickcode@gmail.com>
2023-07-30 20:07:25 +0900

    fix: prisma 스키마 수정

--- src/admin/point/domain/entities/pointTransactionTypes.entity.ts
+++
@@ -1,7 +0,0 @@
-import { PointTransactionTypes } from '@prisma/client';
-
-export class PointTransactionTypesEntity implements PointTransactionTypes {
-  id: number;
-  newId?: number;
-  name: string;
-}
