640ac1fa90b5ce439bb8f7a03bcb706642210522
Kubrick Code <kubrickcode@gmail.com>
2023-08-01 17:21:16 +0900

    feat: 뱃지 구매 로직 트랜잭션 격리 수준 변경

--- src/badge/app/badge.service.ts
+++ src/badge/app/badge.service.ts
@@ -5,8 +5,9 @@ import {
 } from '@badge/domain/dto/buyBadge.app.dto';
 import { IBadgeService } from '@badge/domain/interfaces/badge.service.interface';
 import { IUserBadgeRepository } from '@badge/domain/interfaces/userBadge.repository.interface';
-import { Inject, Injectable } from '@nestjs/common';
+import { ConflictException, Inject, Injectable } from '@nestjs/common';
 import { IPointRepository } from '@point/domain/interfaces/point.repository.interface';
+import { ITransaction } from '@shared/interfaces/transaction.interface';
 
 @Injectable()
 export class BadgeService implements IBadgeService {
@@ -17,20 +18,34 @@ export class BadgeService implements IBadgeService {
     private readonly userBadgeRepository: IUserBadgeRepository,
     @Inject('IBadgeAdminRepository')
     private readonly badgeAdminRepository: IBadgeAdminRepository,
+    @Inject('ITransaction')
+    private readonly transaction: ITransaction,
   ) {}
 
   async buyBadge(req: ReqBuyBadgeAppDto): Promise<ResBuyBadgeAppDto> {
-    const { userId, badgeType } = req;
-    const price = await this.badgeAdminRepository.getBadgePrice(badgeType);
-    const currentPoint = await this.pointRepository.calculateUserPoints(userId);
-    // if (currentPoint - price < 0) throw new Error('포인트 부족');
-    await this.pointRepository.createPointLog(
-      userId,
-      'SPENT',
-      `${badgeType} 구매`,
-      -price,
-    );
-    await this.userBadgeRepository.createUserBadgeLog(userId, badgeType);
-    return { message: '뱃지 구매 성공' };
+    try {
+      await this.transaction.beginTransaction('SERIALIZABLE');
+
+      const { userId, badgeType } = req;
+      const price = await this.badgeAdminRepository.getBadgePrice(badgeType);
+      const currentPoint = await this.pointRepository.calculateUserPoints(
+        userId,
+      );
+      if (currentPoint - price < 0) throw new ConflictException('포인트 부족');
+
+      await this.pointRepository.createPointLog(
+        userId,
+        'SPENT',
+        `${badgeType} 구매`,
+        -price,
+      );
+      await this.userBadgeRepository.createUserBadgeLog(userId, badgeType);
+
+      await this.transaction.commitTransaction();
+      return { message: '뱃지 구매 성공' };
+    } catch (error) {
+      await this.transaction.rollbackTransaction();
+      throw error;
+    }
   }
 }
