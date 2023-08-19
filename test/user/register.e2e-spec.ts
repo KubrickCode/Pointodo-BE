21b54f5bf946fced5e4e32be3ac598c854874c39
Kubrick Code <kubrickcode@gmail.com>
2023-08-20 01:26:40 +0900

    feat: e2e 리퀘스트 함수 구성

--- test/user/register.e2e-spec.ts
+++ test/user/register.e2e-spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { AppModule } from '../../src/app.module';
 import { REGISTER_SUCCESS_MESSAGE } from '@shared/messages/user/user.messages';
+import { USER_ALREADY_EXIST } from '@shared/messages/user/user.errors';
+import {
+  VALIDATE_EMAIL,
+  VALIDATE_PASSWORD,
+} from '@shared/messages/auth/auth.messages';
+import { requestE2E } from '../request.e2e';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
+  const path = '/user/register';
+  const body = { email: 'test@test.com', password: 'test1234!@' };
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,20 +20,46 @@ describe('UserController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
     await app.init();
   });
 
   afterAll(async () => {
+    const loginResult = await requestE2E(app, '/auth/login', 'post', 201, body);
+    await requestE2E(
+      app,
+      '/user',
+      'delete',
+      200,
+      null,
+      loginResult.body.accessToken,
+    );
+
     await app.close();
   });
 
   it('회원가입 e2e 테스트', async () => {
-    const response = await request(app.getHttpServer())
-      .post('/user/register')
-      .send({ email: 'test9@test.com', password: 'test1234!@' })
-      .expect(201);
+    const response = await requestE2E(app, path, 'post', 201, body);
 
-    expect(response.body).toHaveProperty('message');
     expect(response.body.message).toEqual(REGISTER_SUCCESS_MESSAGE);
+  });
+
+  it('회원가입 중복 e2e 테스트', async () => {
+    const response = await requestE2E(app, path, 'post', 409, body);
+
+    expect(response.body.statusCode).toEqual(409);
+    expect(response.body.message).toEqual(USER_ALREADY_EXIST);
+    expect(response.body.path).toEqual(path);
+  });
+
+  it('회원가입 유효성 검사 실패 e2e 테스트', async () => {
+    const response = await requestE2E(app, path, 'post', 400, {
+      email: 'qwe',
+      password: 'qwe',
+    });
+
+    expect(response.body.statusCode).toEqual(400);
+    expect(response.body.message).toEqual([VALIDATE_EMAIL, VALIDATE_PASSWORD]);
+    expect(response.body.path).toEqual(path);
   });
 });
