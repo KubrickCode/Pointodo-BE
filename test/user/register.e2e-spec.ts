6fbed189ff2e96e817b1dee0f75f2da3f6754d66
Kubrick Code <kubrickcode@gmail.com>
2023-08-20 00:46:57 +0900

    feat: 회원가입 e2e테스트 및 인프라 테스트 구축

--- test/user/register.e2e-spec.ts
+++ test/user/register.e2e-spec.ts
@@ -2,11 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../../src/app.module';
+import { REGISTER_SUCCESS_MESSAGE } from '@shared/messages/user/user.messages';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,17 +16,17 @@ describe('UserController (e2e)', () => {
     await app.init();
   });
 
-  it('회원가입 e2e 테스트', async () => {
-    return request(app.getHttpServer())
-      .post('/user/register')
-      .send({ email: 'test7@test.com', password: 'test1234!@' })
-      .expect(201)
-      .then((response) => {
-        expect(response.body).toHaveProperty('message');
-      });
-  });
-
   afterAll(async () => {
     await app.close();
+  });
+
+  it('회원가입 e2e 테스트', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/user/register')
+      .send({ email: 'test9@test.com', password: 'test1234!@' })
+      .expect(201);
+
+    expect(response.body).toHaveProperty('message');
+    expect(response.body.message).toEqual(REGISTER_SUCCESS_MESSAGE);
   });
 });
