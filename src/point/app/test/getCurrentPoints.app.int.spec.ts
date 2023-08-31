7fbaccdd41b63a28314604bc4b9cb4a6107fc3dc
Kubrick Code <kubrickcode@gmail.com>
2023-08-31 16:55:17 +0900

    feat: 유저 포인트 반환 app 통합테스트 구축 완료

---
+++ src/point/app/test/getCurrentPoints.app.int.spec.ts
@@ -0,0 +1,37 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TEST1_USER_LOCAL } from '@shared/test/userMockData';
+import { PointService } from '../point.service';
+import { pointServiceTestModuleOptions } from './pointService.test.option';
+import {
+  ReqGetCurrentPointsAppDto,
+  ResGetCurrentPointsAppDto,
+} from '@point/domain/dto/getCurrentPoints.app.dto';
+
+describe('getCurrentPoints', () => {
+  let pointService: PointService;
+  let module: TestingModule;
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule(
+      pointServiceTestModuleOptions,
+    ).compile();
+
+    pointService = module.get<PointService>(PointService);
+
+    await module.init();
+  });
+
+  afterAll(async () => {
+    await module.close();
+  });
+
+  it('유저 포인트 잔액 요청 성공', async () => {
+    const request: ReqGetCurrentPointsAppDto = {
+      userId: TEST1_USER_LOCAL.id,
+    };
+
+    const result = await pointService.getCurrentPoints(request);
+    expect(result).toBeInstanceOf(ResGetCurrentPointsAppDto);
+    expect(result.points).toEqual(0);
+  });
+});
