49f9946aca3f027248b01dc612e4256d9f7b90b4
Kubrick Code <kubrickcode@gmail.com>
2023-09-02 20:19:57 +0900

    fix: createBadge 앱 통합 테스트 수정

---
+++ src/shared/test/badgeMockData.ts
@@ -0,0 +1,12 @@
+import { BadgeType_ } from '@admin/badge/domain/entities/badge.entity';
+
+const badgeTypes: BadgeType_[] = ['NORMAL', 'ACHIEVEMENT'];
+const randomIndex = Math.floor(Math.random() * badgeTypes.length);
+
+export const mockBadge = {
+  name: 'test',
+  description: 'test',
+  iconLink: 'test',
+  type: badgeTypes[randomIndex],
+  price: 1000,
+};
