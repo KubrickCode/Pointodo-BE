f9a5f5f89781119eb56a5023c8f7af8d6d1a5132
Kubrick Code <kubrickcode@gmail.com>
2023-09-02 20:29:12 +0900

    fix: createBadge 도메인 및 인프라 테스트 수정

--- src/admin/badge/domain/test/infrastructure/createBadge.domain.infra.unit.spec.ts
+++ src/admin/badge/domain/test/infrastructure/createBadge.domain.infra.unit.spec.ts
@@ -1,27 +1,15 @@
 import { IBadgeAdminRepository } from '../../interfaces/badge.admin.repository.interface';
 import { mockBadgeAdminRepository } from './badgeAdminRepository.mock';
-import { BadgeEntity, BadgeType_ } from '../../entities/badge.entity';
+import { BadgeEntity } from '../../entities/badge.entity';
+import { mockBadge } from '@shared/test/badgeMockData';
 
 describe('createBadge', () => {
   const badgeAdminRepository: IBadgeAdminRepository = mockBadgeAdminRepository;
 
   it('뱃지 생성 성공 - NORMAL,ACHIEVEMENT', async () => {
-    const badgeTypes: BadgeType_[] = ['NORMAL', 'ACHIEVEMENT'];
-    const randomIndex = Math.floor(Math.random() * badgeTypes.length);
-
-    const name = 'test';
-    const description = 'test';
-    const iconLink = 'test';
-    const type: BadgeType_ = badgeTypes[randomIndex];
-    const price = 100;
-
     const expectedResult: BadgeEntity = {
       id: 1,
-      name,
-      description,
-      iconLink,
-      type,
-      price,
+      ...mockBadge,
     };
 
     jest
@@ -29,37 +17,27 @@ describe('createBadge', () => {
       .mockResolvedValue(expectedResult);
 
     const result = await badgeAdminRepository.createBadge(
-      name,
-      description,
-      iconLink,
-      type,
-      price,
+      mockBadge.name,
+      mockBadge.description,
+      mockBadge.iconLink,
+      mockBadge.type,
+      mockBadge.price,
     );
 
     expect(result).toEqual(expectedResult);
     expect(badgeAdminRepository.createBadge).toHaveBeenCalledWith(
-      name,
-      description,
-      iconLink,
-      type,
-      price,
+      mockBadge.name,
+      mockBadge.description,
+      mockBadge.iconLink,
+      mockBadge.type,
+      mockBadge.price,
     );
   });
 
   it('뱃지 생성 성공 - SPECIAL', async () => {
-    const name = 'test';
-    const description = 'test';
-    const iconLink = 'test';
-    const type: BadgeType_ = 'SPECIAL';
-    const price = null;
-
     const expectedResult: BadgeEntity = {
       id: 1,
-      name,
-      description,
-      iconLink,
-      type,
-      price,
+      ...mockBadge,
     };
 
     jest
@@ -67,18 +45,18 @@ describe('createBadge', () => {
       .mockResolvedValue(expectedResult);
 
     const result = await badgeAdminRepository.createBadge(
-      name,
-      description,
-      iconLink,
-      type,
+      mockBadge.name,
+      mockBadge.description,
+      mockBadge.iconLink,
+      mockBadge.type,
     );
 
     expect(result).toEqual(expectedResult);
     expect(badgeAdminRepository.createBadge).toHaveBeenCalledWith(
-      name,
-      description,
-      iconLink,
-      type,
+      mockBadge.name,
+      mockBadge.description,
+      mockBadge.iconLink,
+      mockBadge.type,
     );
   });
 });
