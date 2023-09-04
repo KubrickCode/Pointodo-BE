98e4116110a08c33bced3eb844aab68fa2c4e615
Kubrick Code <kubrickcode@gmail.com>
2023-09-04 14:23:01 +0900

    fix: 뱃지 엔티티 및 유저 뱃지 리스트 반환 앱 dto 수정

--- src/badge/domain/dto/getUserBadgeList.app.dto.ts
+++ src/badge/domain/dto/getUserBadgeList.app.dto.ts
@@ -1,9 +1,10 @@
 import { UUID } from 'crypto';
+import { Expose } from 'class-transformer';
 
 export class ReqGetUserBadgeListAppDto {
   readonly userId: UUID;
 }
 
 export class ResGetUserBadgeListAppDto {
-  readonly badgeId: number;
+  @Expose() readonly badgeId: number;
 }
